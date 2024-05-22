41603b256662ac1aeebb6889d5a6d652c785714c
JoshRosner101 <joshrosner0827@gmail.com>
2024-05-22 17:19:33 -0400

    Created a modified voronoi noise shader

---
+++ Shaders/tiled_voronoi_noise.glsl
@@ -0,0 +1,57 @@
+//This is a variant of the voronoi noise function that shows whole colors, not gradients
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+vec2 random (vec2 uv) {
+    return vec2(fract(sin(dot(uv.xy, vec2(349.2321,353.931)))* 32219.131));
+}
+
+void main()
+{
+    //xy coordinates divided by xy resolution
+    vec2 uv = gl_FragCoord.xy/u_resolution.xy;
+
+    vec3 color = vec3(0.0);
+
+    //Scale the uv
+    uv.x*= 4.45;
+    uv.y*= 6.0;
+
+    //Create the tiles
+    vec2 currentGridId = floor(uv);
+    vec2 currentGridCoord = fract(uv);
+
+    //This time keep the point as a vector to create colors from
+    vec2 minPoint;
+    float minDistFromPixel = 1.0;
+
+    for(float i = -1.0; i <= 1.0; i++) {
+        for(float j = -1.0; j <= 1.0; j++) {
+            vec2 adjGridCoords = vec2(i,j);
+            vec2 pointOnAdjGrid = random(currentGridId + adjGridCoords);
+
+            //Put a variable where 0.0 is
+            pointOnAdjGrid = 0.5 + 0.5*sin(0.54 * u_mouse.y/u_resolution.y - 0.4 * + u_mouse.x/u_resolution.x+ 6.2831 * random(currentGridId + adjGridCoords));
+
+            vec2 diff = adjGridCoords + pointOnAdjGrid - currentGridCoord;
+            float distance = length(diff);
+            //This makes the colors snap to the individual tiles
+            if(distance < minDistFromPixel) {
+                //minDistFromPixel shouldn't be greater than the distance
+                minDistFromPixel = distance;
+                //If this isn't the if statement, it will create colors for each TILE
+                minPoint = pointOnAdjGrid;
+            }
+        }
+    }
+
+    color += dot(minPoint,vec2(.3,.6));
+
+
+    gl_FragColor = vec4(color,1.0);
+}
