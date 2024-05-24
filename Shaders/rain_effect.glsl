13d74757d4c2de03162cde5aab8b59eb74085dd9
JoshRosner101 <joshrosner0827@gmail.com>
2024-05-23 21:35:19 -0400

    Tweaked some numbers in rain shader

--- Shaders/rain_effect.glsl
+++ Shaders/rain_effect.glsl
@@ -5,9 +5,19 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 float rain(vec2 uv) {
-    uv.x -= mod(uv.x, 0.01);
-    uv.y -= mod(uv.y, 0.02);
-    float gridUv = (fract(uv.y + (u_time + 1340.0) * abs(cos(uv.x * 2.0)*0.2 - 0.4)));
+    uv.x -= mod(uv.x, 0.0125);
+    uv.y -= mod(uv.y, 0.025);
+
+    /*
+        //This is an alternate version with different numbers
+        float offset = sin(uv.x * 0.025);
+        float speed = abs(cos(uv.x*9.0))*.51 + 0.1;
+  
+        float gridUv = fract(uv.y + u_time*speed + offset);
+        gridUv += fract(uv.x + u_time*speed - offset) * 0.3;
+    */
+
+    float gridUv = (fract(uv.y + (u_time + 1340.0) * abs(cos(uv.x * 2.0))*0.2 - 0.4));
     return 0.08/gridUv;
 }
 
