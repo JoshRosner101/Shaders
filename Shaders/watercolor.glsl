96c2fdff11385c4827752adddf6118d5dd05fc96
JoshRosner101 <joshrosner0827@gmail.com>
2024-05-26 16:20:33 -0400

    Changed watercolor parameters

--- Shaders/watercolor.glsl
+++ Shaders/watercolor.glsl
@@ -13,14 +13,16 @@ void main()
     vec2 uv = gl_FragCoord.xy/u_resolution;
 
     //Scale up the uv
-    uv *= 10.0;
+    uv *= 4.0;
+    // uv.x += u_time;
+    // uv.y += u_time*0.2;
 
-    const float octaves = 8.0;
+    const float octaves = 50.0;
     for(float i = 1.0; i < octaves; i++) {
         uv += vec2(0.25/i * sin(i * uv.y + u_time + 0.3 * i) + 0.8, 1.0/i * sin(i * uv.x + u_time + 0.3 * i)+1.6);
     }
 
     vec3 color = vec3(scaleUp(uv.x), scaleUp(uv.y), scaleUp(uv.x+uv.y) - 0.5);
-
+    color = vec3(mix(color.x, color.y, 0.5));
     gl_FragColor = vec4(color,1.0);
 }
