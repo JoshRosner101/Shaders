ce0c02e3b2f29cd59502d606a8d7c91d1e490ce3
JoshRosner101 <joshrosner0827@gmail.com>
2024-05-29 16:58:38 -0400

    Create twinkle.glsl

---
+++ Shaders/twinkle.glsl
@@ -0,0 +1,24 @@
+//This is the default shader on ShaderToy written in glsl
+precision mediump float;
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+//Pseudo-random number generator
+float Hash21(vec2 uv) {
+    uv = fract(uv*vec2(234.34,435.345));
+    uv += dot(uv, uv+34.23);
+    return fract(uv.x*uv.y - u_time*0.0025);
+}
+
+void main()
+{
+    vec2 uv = gl_FragCoord.xy/u_resolution;
+
+    vec3 color = vec3(0.0);
+    float noise = Hash21(uv);
+    color = vec3(smoothstep(0.99, 1.0, noise));
+
+    gl_FragColor = vec4(color,1.0);
+}
