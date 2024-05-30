04e97b25a04cb26ed159e42e351061bbe563f2ec
JoshRosner101 <joshrosner0827@gmail.com>
2024-05-30 07:42:48 -0400

    Create hexagonal_tiling.glsl

---
+++ Shaders/Tutorials/hexagonal_tiling.glsl
@@ -0,0 +1,48 @@
+//This is an implementation of the hexagonal tiling that was made by following the tutorial at https://www.youtube.com/watch?v=VmrIDyYiJBA
+precision mediump float;
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+float hexDist(vec2 uv) {
+    uv = abs(uv);
+    float c = dot(uv, normalize(vec2(1.0,1.73)));
+    c = max(c, uv.x);
+    return c;
+}
+
+vec4 hexCoords(vec2 uv) {
+    vec2 ratio = vec2(1.0, 1.73);
+    vec2 h = ratio/2.0;
+    vec2 gridUvA = mod(uv, ratio) - h;
+    vec2 gridUvB = mod(uv - h, ratio) - h;
+    vec2 gridUv;
+    if(length(gridUvA) < length(gridUvB)) {
+        gridUv = gridUvA;
+    }
+    else {
+        gridUv = gridUvB;
+    }
+    //this is the hexagonal UV
+    float x = atan(gridUv.x,gridUv.y);
+    float y = 0.5 - hexDist(gridUv);
+    //different for each grid cell
+    vec2 gridId = uv-gridUv;
+    return vec4(x, y, gridId);
+}
+
+void main()
+{
+    vec2 uv = (gl_FragCoord.xy-0.5*u_resolution.xy)/u_resolution.y;
+
+    vec3 color = vec3(0.0);
+
+    uv *= 10.0;
+    vec4 hexCoordinates = hexCoords(uv);
+    float c = smoothstep(0.08, 0.1, hexCoordinates.y*sin(hexCoordinates.z*hexCoordinates.w+u_time));
+    
+    color +=c;
+
+    gl_FragColor = vec4(color,1.0);
+}
