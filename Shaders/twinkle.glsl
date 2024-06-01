cdab250427c0f7e9934614280b3b4992e170f611
JoshRosner101 <joshrosner0827@gmail.com>
2024-05-31 21:09:46 -0400

    Added more sparking to twinkle.glsl

--- Shaders/twinkle.glsl
+++ Shaders/twinkle.glsl
@@ -12,13 +12,19 @@ float Hash21(vec2 uv) {
     return fract(uv.x*uv.y - u_time*0.0005);
 }
 
+float smoothing(float noise) {
+    return smoothstep(0.997, 1.0, noise);
+}
+
 void main()
 {
     vec2 uv = gl_FragCoord.xy/u_resolution;
 
     vec3 color = vec3(0.0);
     float noise = Hash21(uv);
-    color = vec3(smoothstep(0.997, 1.0, noise));
+    float noise2 = Hash21(uv + 2.0);
+    float noise3 = Hash21(uv + 4.0);
+    color = vec3(smoothing(noise) + 0.5* smoothing(noise2) + 0.35* smoothing(noise3));
 
     gl_FragColor = vec4(color,1.0);
 }
