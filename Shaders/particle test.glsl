a3252b4d29efbe8f6418f932d55d4adde9fb6d50
JoshRosner101 <joshrosner0827@gmail.com>
2024-07-13 09:48:47 -0400

    Update particle test.glsl

--- Shaders/particle test.glsl
+++ Shaders/particle test.glsl
@@ -13,12 +13,12 @@ void main()
 {
     // -1.0 to 1.0
     vec2 uv = (2.0*gl_FragCoord.xy-u_resolution.xy)/u_resolution.y;
-    vec2 uv2 = uv;
+    vec2 uv2 = uv * 50.0;
     vec3 color = vec3(0.0);
 
-    uv2 *= 4.0;
-
-    color += vec3(sdCircle(vec2(0.5), fract(uv2 + u_time), 0.3*(1.0-length(uv))));
+    vec2 point = fract(uv2 + u_time);
+    float radius = 0.3*(1.0-length(uv));
+    color += vec3(sdCircle(vec2(0.5), point, radius));
 
     
     gl_FragColor = vec4(color,1.0);
