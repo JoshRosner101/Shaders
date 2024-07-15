7dbfd031d677e2711ea3026b2643b8a0c4efad84
JoshRosner101 <joshrosner0827@gmail.com>
2024-07-15 17:38:24 -0400

    Update particle test.glsl

--- Shaders/particle test.glsl
+++ Shaders/particle test.glsl
@@ -13,8 +13,8 @@ void main()
 {
     // -1.0 to 1.0
     vec2 uv = (2.0*gl_FragCoord.xy-u_resolution.xy)/u_resolution.y;
-    vec2 uv2 = uv * 50.0;
-    vec3 color = vec3(1.0);
+    vec2 uv2 = uv * 10.0;
+    vec3 color = vec3(cos(u_time + uv.yxy + vec3(1.0, 0.0, 2.0)));
 
     vec2 point = fract(uv2 + u_time);
     float radius = 0.3*(1.0-length(uv));
