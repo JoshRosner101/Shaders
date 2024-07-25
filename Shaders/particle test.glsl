c5e362c59c2d504b137193032a7a2791fbd7cb40
JoshRosner101 <joshrosner0827@gmail.com>
2024-07-25 17:12:01 -0400

    Reverted alpha to color in particle test

--- Shaders/particle test.glsl
+++ Shaders/particle test.glsl
@@ -14,7 +14,7 @@ void main()
     // -1.0 to 1.0
     vec2 uv = (2.0*gl_FragCoord.xy-u_resolution.xy)/u_resolution.y;
     vec2 uv2 = uv * 10.0;
-    vec3 color = 0.5+0.5*cos(u_time+uv.xyx+vec3(0,2,4));
+    vec3 color = vec3(0.15);
 
     vec2 movinguv2 = uv2 + u_time;
 
@@ -22,8 +22,7 @@ void main()
     vec2 tile = floor(movinguv2);
 
     float radius = 0.3*(1.0-length(uv));
-    float alpha = 0.0;
-    color += 0.25*sdCircle(vec2(0.5), point, radius/2.0);
+    color += 0.25*sdCircle(vec2(0.5), point, radius/4.0);
 
     //Flips direction for every other circle
     float direction = (mod(tile.x, 2.0)*2.0-1.0)*(mod(tile.y, 2.0)*2.0-1.0);
@@ -33,11 +32,14 @@ void main()
     float angle = 6.283/numCircles;
     for(float i = 0.0; i < numCircles; i++) {
         float theta = i*angle + direction*angle*smoothstep(0.0, 1.0, fract(u_time/1.5*rotationSpeed));
-        alpha += 0.5*sdCircle(vec2(0.5) + radius*vec2(cos(theta), sin(theta)), point, radius);
+        color += 0.5*sdCircle(vec2(0.5) + radius*vec2(cos(theta), sin(theta)), point, radius);
     }
 
     //This adds shading
     color *= length(point);
+
+    //This adds back the color
+    color *= 0.5+0.5*cos(u_time+uv.xyx+vec3(0,2,4));
     
-    gl_FragColor = vec4(color, alpha);
+    gl_FragColor = vec4(color, 1.0);
 }
