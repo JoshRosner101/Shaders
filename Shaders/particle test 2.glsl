27c0336cacd462f7cc845506c928837f2af1536d
JoshRosner101 <joshrosner0827@gmail.com>
2024-08-05 17:42:37 -0400

    Update particle test 2.glsl

--- Shaders/particle test 2.glsl
+++ Shaders/particle test 2.glsl
@@ -19,7 +19,7 @@ void main()
     const float ROTATIONS = 10.0;
     for(float j = 0.0; j < ROTATIONS; j++) {
         for(float i = 0.0; i < PARTICLE_COUNT; i++) {
-            color += vec3(sdCircle(0.5*vec2(cos(j*3.1415/ROTATIONS)*cos(i*6.283/PARTICLE_COUNT),sin(i*6.283/PARTICLE_COUNT)), uv, 0.02));
+            color += vec3(sdCircle(0.5*vec2(cos(j*3.1415/ROTATIONS)*cos(i*6.283/PARTICLE_COUNT),sin(j*3.1415/ROTATIONS)*sin(i*6.283/PARTICLE_COUNT)), uv, 0.02));
         }
     }
 
