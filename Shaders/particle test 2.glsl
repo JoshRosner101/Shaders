fbca897cb248acccb260af6dd4960e616e10a67f
JoshRosner101 <joshrosner0827@gmail.com>
2024-08-17 20:27:33 -0400

    Update particle test 2.glsl

--- Shaders/particle test 2.glsl
+++ Shaders/particle test 2.glsl
@@ -63,13 +63,13 @@ void main()
     const float SCALE = 0.5;
     const float DISPLACE_X = 0.0;
     const float DISPLACE_Y = -0.5;
-    float RADIUS = 0.5;
-    const float SKEW = 1.0;
+    float RADIUS = 2.5;
+    const float SKEW = 2.0;
     for(float j = 0.0; j < HEIGHT; j++) {
         for(float i = 0.0; i < PARTICLE_COUNT; i++) {
             //RADIUS = sin(j*3.1415/8.0);
 			//RADIUS = (i*i)/(j*j);
-            // RADIUS = i/PARTICLE_COUNT;
+            RADIUS = i/PARTICLE_COUNT;
             float angle = i*6.283/PARTICLE_COUNT;
             vec3 uniqueColor = colorize(j/HEIGHT);
             vec3 particles = vec3(sdCircle(0.5*vec2(RADIUS*cos(angle + u_time) + DISPLACE_X,j/HEIGHT*SCALE + 0.5*sin(angle + u_time*SKEW) + DISPLACE_Y), uv, 0.02));
